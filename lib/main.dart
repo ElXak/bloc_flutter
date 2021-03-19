c8e3b9b5a72e6cc5f068017e424d4680326eee0e
ElXak <gelxak@gmail.com>
2021-03-20 01:52:05 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'color_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: BlocProvider(
+        create: (context) => ColorBloc(),
+        child: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: close_sinks
+    ColorBloc _bloc = BlocProvider.of<ColorBloc>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BloC with flutter_bloc'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: BlocBuilder<ColorBloc, Color>(
+          builder: (context, currentColor) => AnimatedContainer(
+            height: 100,
+            width: 100,
+            color: currentColor,
+            duration: Duration(milliseconds: 500),
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            backgroundColor: Colors.red,
+            onPressed: () {
+              _bloc.add(ColorEvent.event_red);
+            },
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              _bloc.add(ColorEvent.event_green);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
